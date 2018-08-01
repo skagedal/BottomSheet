bb6a4f8ecfef6cb4c995dcdbbde59caf806a54e0
Simon Kågedal Reimer <skagedal@gmail.com>
2018-08-01 21:46:39 +0200

    Make background sheet look a little nicer

--- BottomSheet/BottomSheetBackgroundView.swift
+++ BottomSheet/BottomSheetBackgroundView.swift
@@ -4,16 +4,30 @@
 
 import UIKit
 
+private let borderWidth: CGFloat = 1
+private let cornerRadius: CGFloat = 12
+
 class BottomSheetBackgroundView: UIView {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         backgroundColor = .white
+        layer.cornerRadius = cornerRadius
+        layer.borderColor = UIColor.lightGray.cgColor
+        layer.borderWidth = borderWidth
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError()
     }
-    
+ 
+    override func layoutSubviews() {
+        super.layoutSubviews()
+ 
+        // Make sure border isn't visible
+        layer.bounds = CGRect(origin: bounds.origin,
+                              size: CGSize(width: bounds.size.width + borderWidth * 2,
+                                           height: bounds.size.height))
+    }
 }
